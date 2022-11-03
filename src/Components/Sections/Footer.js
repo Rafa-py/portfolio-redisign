2c4800f2525b58f7917b98c1859e45de8aea4c36
Rafa <rafadevjs@gmail.com>
2022-11-03 02:27:01 -0300

    style(footer): Create the footer section

--- src/Components/Sections/Footer.js
+++ src/Components/Sections/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styles from "./Footer.module.scss";
+import copy from "/home/rafa/porfolio/src/Assets/copy.png";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={`container`}>
+      <div className={styles.footer}>
+        <p><img className={styles.img} src={copy} alt="Icone Copyright"/>2022 rafadev.tech</p>
+        <p>Powered by Rafa</p>
+      </div>
     </div>
   )
 }
