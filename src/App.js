e4324d3965f0c18c2e5927b0af75c29fa0b19a6a
Rafa <rafadevjs@gmail.com>
2023-01-04 12:53:49 -0300

    feat(head): Create the meta tag

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import Header from "./Components/Sections/Header";
 import Footer from "./Components/Sections/Footer";
 import Home from "./Components//Sections/Home";
 import ProjectsAll from "./Components//Sections/ProjectsAll";
+import { Helmet } from "react-helmet";
+import imgBanner from "./Assets/imgBanner.png";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Rafa</title>
+        <meta name="description" content="Portfólio do Rafa. Projetos de estudos e Freelancer"/>
+        <meta name="author" content="Rafael S. Pinto - Front End Developer"/>
+        <meta name="keywords" content="programação, html, portifólio, front end, react, javascript, css, desenvolvedor, programador, developer, "/>
+        <meta property="og:image" content={imgBanner} />
+      </Helmet>
       <BrowserRouter>
         <UserStorage>
           <Header />
